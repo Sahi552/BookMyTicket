4f1d37efe708e37b6cdaab0a0baf55e90171bc46
Sahi552 <sahithiyani280@gmail.com>
2025-01-27 23:53:20 +0530

    sixth commit

--- lib/view/widgets/popular_event.dart
+++ lib/view/widgets/popular_event.dart
@@ -1,4 +1,5 @@
 import 'package:bookmyticket/utils/assest.dart';
+import 'package:bookmyticket/utils/theme.dart';
 import 'package:flutter/material.dart';
 
 class PopularEvent extends StatelessWidget {
@@ -9,25 +10,105 @@ class PopularEvent extends StatelessWidget {
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
       child: Row(
+        spacing: 10,
         children: [
-          Column(
-            children: [
-              Container(
-                width: 144,
-                height: 186,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  image: DecorationImage(
-                    image: AssetImage(
-                      Assest.popevent1,
-                    ),
-                  ),
-                ),
-              )
-            ],
+          PopEvents(
+            image: Assest.popevent1,
+            area: "Bangalore karnataka \nconcert",
+            concert: "MUSIC KA 24",
+            month: "Jan",
+            date: 20,
+          ),
+          PopEvents(
+            image: Assest.popevent2,
+            area: "Bangalore karnataka \nconcert",
+            concert: "CAR SHOW KA",
+            month: "Jan",
+            date: 20,
+          ),
+          PopEvents(
+            image: Assest.popevent3,
+            area: "Bangalore karnataka \nconcert",
+            concert: "Comedy KA 24",
+            month: "Jan",
+            date: 20,
           )
         ],
       ),
+    );
+  }
+}
+
+class PopEvents extends StatelessWidget {
+  final String image;
+  final String concert;
+  final String area;
+  final String month;
+  final int date;
+  const PopEvents(
+      {super.key,
+      required this.image,
+      required this.concert,
+      required this.area,
+      required this.month,
+      required this.date});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Stack(
+          children: [
+            Container(
+              width: 150,
+              height: 200,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25),
+                image: DecorationImage(
+                  image: AssetImage(
+                    image,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 20,
+              left: 18,
+              child: Container(
+                height: 42,
+                width: 40,
+                decoration: BoxDecoration(
+                  color: Colors.white54,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      month,
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.w400),
+                    ),
+                    Text(
+                      date.toString(),
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.w800),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+        Text(
+          concert,
+          style: AppTheme.heading,
+        ),
+        Text(
+          area,
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.w300),
+        ),
+      ],
     );
   }
 }
